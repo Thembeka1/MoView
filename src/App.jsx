073d9f162f05b32a539b2078eaf3b3b328d4e72e
Thembeka <ttheh98@gmail.com>
2026-07-27 15:03:58 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Movies from "./components/Movies";
 import MovieDetails from "./components/MovieDetails";
